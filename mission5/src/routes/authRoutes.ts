f891a5918861dada964113f9d708bc20f34dce55
Calvin Lee <jinwoo0451@naver.com>
2025-10-05 14:17:37 +0700

    이제 남은 것: prisma model 적절하게 다 추가하고 npx prisma generate 한 뒤에 prisma error 고쳐서 typecheck로 컴파일 에러 없을때까지 고치기

--- mission5/src/routes/authRoutes.ts
+++ mission5/src/routes/authRoutes.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.post("/logout", logout);
+// router.post("/logout", logout);
 
 // 회원가입
 async function register(req: Request, res: Response): Promise<Response> {
